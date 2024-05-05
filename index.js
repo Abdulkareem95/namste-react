4ecf1643a1587ba669f1ecf7251ecfba661d5e07
Abdulkareem95 <abdulkareemshaik95@gmail.com>
2024-05-05 14:00:01 +0530

    episode-1

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * <div id ="parent">
+ * <div id="child">
+ * <h1> I am h1 tag</h1>
+ * </div>
+ * </div>
+ */
+
+const ele = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am h1 tag")
+  )
+);
+
+console.log(ele);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(ele);
